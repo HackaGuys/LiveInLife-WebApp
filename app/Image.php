84a4a32e4e7b77ba01338e52afadccdd2c34eacc
Jacky Lui <jackycodes@gmail.com>
2016-02-27 22:28:18 -0800

    updated models

--- app/Image.php
+++ app/Image.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+    protected $table = 'images';
+    public $timestamps = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'post_id',
+        'filename',
+        'thumbnail_filename'
+    ];
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $table = 'posts';
+    public $timestamps = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'address',
+        'city',
+        'province',
+        'zip',
+        'bedrooms',
+        'sqfeet',
+        'price',
+        'description'
+    ];
 }
