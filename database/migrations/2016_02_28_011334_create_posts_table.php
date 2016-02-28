2e5a1f342889277236c35beccaaeeb4acc03491a
Jacky Lui <jackycodes@gmail.com>
2016-02-27 22:22:26 -0800

    added images table

--- database/migrations/2016_02_28_011334_create_posts_table.php
+++ database/migrations/2016_02_28_011334_create_posts_table.php
@@ -14,6 +14,10 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+
             $table->string('address');
             $table->string('city');
             $table->string('province');
@@ -21,6 +25,9 @@ class CreatePostsTable extends Migration
             $table->integer('bedrooms');
             $table->integer('sqfeet');
             $table->integer('price');
+
+            $table->text('description');
+
             $table->timestamps();
         });
     }
